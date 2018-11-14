d920a9e8f1cad6f21b096a4f3cedbe546ab2583c
Wei-ju Wu <weiju.wu@gmail.com>
2018-11-14 12:55:16 -0800

    removed the tests scripts
    avoiding future confusions because they don't do anything

--- server/tests.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import mirv_db
-
-
-if __name__ == '__main__':
-    old = datetime.datetime.now()
-    uuids = mirv_db.find_old_jobs(old)
-    for uuid in uuids:
-        mirv_db.delete_job(uuid)
